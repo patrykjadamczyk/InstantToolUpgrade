67b2990b90ca6c81e1f8c1c2f4ca00b0d85cdc93
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-01-14 13:44:28 +0100

    Instant Tool Upgrade - Initial Version

---
+++ InstantToolUpgrade/ModEntry.cs
@@ -0,0 +1,39 @@
+﻿namespace InstantToolUpgrade;
+
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using System;
+
+internal sealed class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+    }
+
+    private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
+    {
+        // Any time the player submits a tool to be upgraded...
+        if (Game1.player.daysLeftForToolUpgrade.Value > 0)
+        {
+            // Check to see if it's a genericTool...
+            if (Game1.player.toolBeingUpgraded.Value is StardewValley.Tools.GenericTool genericTool)
+            {
+                // ...and upgrade it.
+                genericTool.actionWhenClaimed();
+            }
+            else
+            {
+                // Otherwise give the player their new tool.
+                Game1.player.addItemToInventory(Game1.player.toolBeingUpgraded.Value);                    
+            }
+                
+            // Finally, cancel the queued upgrade.
+            Game1.player.toolBeingUpgraded.Value = null;
+            Game1.player.daysLeftForToolUpgrade.Value = 0;
+        }
+    }
+}
+
